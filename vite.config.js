65ad7f9c08ae0b72f5f22fbfb8e3a6852fd9513d
pelz <pelz@gmail.com>
2024-12-29 01:44:22 -0800

    Updated features and made corrections

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Task-management/', // Add this linenpm run deploy
   plugins: [react()],
 })
